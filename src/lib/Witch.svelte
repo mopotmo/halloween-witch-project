dec2750c62ec0030b7633ddd49182689f3fa535a
Manu Robledo <manu@insfx.com>
2023-10-28 21:25:10 +0200

    Add witch laughing sound

--- src/lib/Witch.svelte
+++ src/lib/Witch.svelte
@@ -1,10 +1,12 @@
 <script>
     import WitchImage from "../assets/witch.png";
+    import WitchLaughing from "../assets/witch-laughs.mp3";
 
     export let width = 326;
 </script>
 
 <img src={WitchImage} alt="Hexe" {width} />
+<audio src={WitchLaughing} autoplay />
 
 <style>
     img {
