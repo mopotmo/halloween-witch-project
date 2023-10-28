94796887bf4a2899bc8b3a8a6dd942e855814d84
Manu Robledo <manu@insfx.com>
2023-10-28 21:53:20 +0200

    Let witch laugh in random intervals

--- src/lib/Witch.svelte
+++ src/lib/Witch.svelte
@@ -1,15 +1,29 @@
 <script>
     import WitchImage from "../assets/witch.png";
     import WitchLaughing from "../assets/witch-laughs.mp3";
+    import { onMount } from "svelte";
 
     export let width = 326;
+    export let minPause = 2;
+    export let maxPause = 30;
+
+    let audio;
+
+    const schedulePlay = () => {
+        const pause = Math.random() * (maxPause - minPause) + minPause;
+        setTimeout(() => {
+            audio.play();
+            schedulePlay();
+        }, pause * 1000);
+        console.log("Scheduled next play in " + pause + " seconds");
+    };
 </script>
 
 <img src={WitchImage} alt="Hexe" {width} />
-<audio src={WitchLaughing} autoplay />
+<audio bind:this={audio} src={WitchLaughing} autoplay on:ended={schedulePlay} />
 
 <style>
     img {
-        filter: drop-shadow(5px 5px 5px #222);
+        filter: drop-shadow(8px 8px 8px #222);
     }
 </style>
